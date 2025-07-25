7c4f4dbaf347d3a657dde548f83f740e8cb3af0b
abinjoseph2050 <abinjoseph2050@gmail.com>
2025-07-25 09:00:43 +0530

    Experience

--- src/constants/index.js
+++ src/constants/index.js
@@ -162,8 +162,8 @@ const expCards = [
             "Adrian brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
         imgPath: "/images/exp1.png",
         logoPath: "/images/logo1.png",
-        title: "Frontend Developer",
-        date: "January 2023 - Present",
+        title: "BTech Student",
+        date: "August 2017 - July 2021",
         responsibilities: [
             "Developed and maintained user-facing features for the Hostinger website.",
             "Collaborated closely with UI/UX designers to ensure seamless user experiences.",
@@ -175,8 +175,8 @@ const expCards = [
             "Adrian’s contributions to Docker's web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
         imgPath: "/images/exp2.png",
         logoPath: "/images/logo2.png",
-        title: "Full Stack Developer",
-        date: "June 2020 - December 2023",
+        title: "Software Developer Trainee",
+        date: "September 2021 - October 2022",
         responsibilities: [
             "Led the development of Docker's web applications, focusing on scalability.",
             "Worked with backend engineers to integrate APIs seamlessly with the frontend.",
@@ -188,8 +188,34 @@ const expCards = [
             "Adrian’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.",
         imgPath: "/images/exp3.png",
         logoPath: "/images/logo3.png",
-        title: "React Native Developer",
-        date: "March 2019 - May 2020",
+        title: "Junior Software Developer",
+        date: "October 2022 - October 2023",
+        responsibilities: [
+            "Built cross-platform mobile apps using React Native, integrating with Appwrite's backend services.",
+            "Improved app performance and user experience through code optimization and testing.",
+            "Coordinated with the product team to implement features based on feedback.",
+        ],
+    },
+    {
+        review:
+            "Adrian’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.",
+        imgPath: "/images/exp3.png",
+        logoPath: "/images/logo3.png",
+        title: "Software Developer",
+        date: "October 2023 - August 2024",
+        responsibilities: [
+            "Built cross-platform mobile apps using React Native, integrating with Appwrite's backend services.",
+            "Improved app performance and user experience through code optimization and testing.",
+            "Coordinated with the product team to implement features based on feedback.",
+        ],
+    },
+    {
+        review:
+            "Adrian’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.",
+        imgPath: "/images/exp3.png",
+        logoPath: "/images/logo3.png",
+        title: "Associate Consultant",
+        date: "September 2024 - Present",
         responsibilities: [
             "Built cross-platform mobile apps using React Native, integrating with Appwrite's backend services.",
             "Improved app performance and user experience through code optimization and testing.",
